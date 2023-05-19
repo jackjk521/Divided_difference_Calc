0250dc6a426b0d0373dafac60e49c125381fc25a
j-Tinga <justintinga321@gmail.com>
2023-05-19 19:21:15 +0800

    Font sizes for smoller screens

--- src/components/sideNavigation.js
+++ src/components/sideNavigation.js
@@ -3,9 +3,9 @@ const SideNavigation = () => {
     <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark  ">
         <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
             <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                <span class="fs-5 d-none d-sm-inline">Divided Differences</span>
+                <span class="fs-3 d-none d-sm-inline">Divided Differences</span>
             </a>
-            <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start sticky-top" id="menu">
+            <ul class="nav nav-pills flex-column mb-sm-0 mb-0 align-items-center align-items-sm-start sticky-top fs-4" id="menu">
                 <li class="nav-item">
                     <a href="#introduction" class="nav-link align-middle px-0">
                     <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Introduction</span>
@@ -29,17 +29,20 @@ const SideNavigation = () => {
             </ul>
 
             <hr/>
-                <div class="dropdown pb-4 fixed-bottom">
-                    MAT 4201 <br/>
-                    Group  8 Members:
-                        <ul>
-                            <li>Amaba, Carlina</li>
-                            <li>Chu, Jed Abner</li>
-                            <li>Colina, Luke Abram</li>
-                            <li>Pasana, Jon Francis</li>
-                            <li>Tinga, Justin Dominic</li>
-                        </ul>
-                </div>
+        <div class="container">
+  
+            <div class="lead dropdown pb-4 custom-dropdown fixed-bottom ">
+            MAT 4201 <br/>
+            Group  8 Members:
+            <ul>
+                <li>Amaba, Carlina</li>
+                <li>Chu, Jed Abner</li>
+                <li>Colina, Luke Abram</li>
+                <li>Pasana, Jon Francis</li>
+                <li>Tinga, Justin Dominic</li>
+            </ul>
+            </div>
+        </div>
         </div>
     </div>
         
